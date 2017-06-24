f7d85d3a6975bd99fa121bd9dac551790f6338aa
EgimenoS <ergisan@gmail.com>
2017-06-24 17:28:21 +0200

    Imported key

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { youtubeKey } from 'keys'
+import API_KEY from './keys'
 
 
 //create a neew component. This component should produce some HTML
 
 const App = ()  => {
-    return  <div>Hi!</div>;
+    return  <div>Hi </div>;
 }
 
 //take this component's generated HTML and put it on the page(DOM)
