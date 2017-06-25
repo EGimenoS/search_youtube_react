69c16c501b3ef531995b77c7d41c5137c9e563dd
EgimenoS <ergisan@gmail.com>
2017-06-25 19:24:03 +0200

    implemented video_list_item component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import API_KEY from './keys'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list';
 
+//create a new component. This component should produce some HTML
 
-//create a neew component. This component should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = ()  => {
-    return  <div>Hi </div>;
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return  (
+        <div>
+            <SearchBar />
+            <VideoList videos={ this.state.videos } />
+        </div>
+        );
+    }
 }
 
 //take this component's generated HTML and put it on the page(DOM)
